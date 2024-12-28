43fcb24a0159975b65cd850c74b1a061c840a340
ayaginidy23 <aya.tamer.ginidy2@gmail.com>
2024-12-28 16:37:01 +0200

    Delete SummarizationService directory

--- SummarizationService/text_summarization/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TextSummrizationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'text_summarization'
--- SummarizationService/text_summarization/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('summarize', views.summarized_text), # post method
-    path('status/<int:text_id>', views.get_status), # get method
-    path('home', views.home),
-]
